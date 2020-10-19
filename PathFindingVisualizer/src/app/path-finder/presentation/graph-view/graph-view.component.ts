84666c97c87a80ce4567ecec3c396bcdf0ff210e
Philipp Butz <philipp.butz92@gmail.com>
2020-10-19 17:13:05 +0200

    initial commit showing graph

--- PathFindingVisualizer/src/app/path-finder/presentation/graph-view/graph-view.component.ts
+++ PathFindingVisualizer/src/app/path-finder/presentation/graph-view/graph-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import * as p5 from 'p5';
 
 @Component({
   selector: 'app-graph-view',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2) { }
 
   ngOnInit(): void {
+
+    const size = 450;
+
+    const graph = new p5(p => {
+      let x = 100;
+      let y = 100;
+
+      p.setup = () => {
+        p.createCanvas(size, size);
+      };
+
+      p.draw = () => {
+        p.background(0);
+        p.fill(255);
+        p.rect(x, y, 50, 50);
+      };
+    }, this.el.nativeElement);
   }
 
 }
