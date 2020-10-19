f926adfc89deac589d60a64a2a71ff21ac3c619a
Philipp Butz <philipp.butz@averbis.com>
2020-10-19 17:13:05 +0200

    initial commit showing graph

--- PathFindingVisualizer/src/app/path-finder/presentation/graph-controls/graph-controls.component.ts
+++ PathFindingVisualizer/src/app/path-finder/presentation/graph-controls/graph-controls.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import * as p5 from 'p5';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-graph-controls',
@@ -8,30 +7,10 @@ import * as p5 from 'p5';
 })
 export class GraphControlsComponent implements OnInit {
 
-
-  @ViewChild('graph', { static: false }) graph: ElementRef<HTMLCanvasElement>;
-
-
   constructor() { }
 
   ngOnInit(): void {
-    const sketch = (s) => {
 
-      s.preload = () => {
-        // preload code
-      };
-
-      s.setup = () => {
-        s.createCanvas(800, 800);
-      };
-
-      s.draw = () => {
-        s.background(255);
-        s.rect(100, 100, 100, 100);
-      };
-    };
-
-    this.graph.nativeElement = new p5(sketch);
   }
 
 }
