960ac79d11ce2fe9b33f03545751ea84003dafa4
Philipp Butz <philipp.butz92@gmail.com>
2020-12-28 14:15:37 +0100

    removed unused class

--- PathFindingVisualizer/src/app/model/AlgorithmEvent.ts
+++
@@ -1,6 +0,0 @@
-import { GraphCellConstraint } from './GraphCell';
-import { RowColumnPair } from './RowColumnPair';
-
-export class AlgorithmEvent {
-  constructor(public cell: RowColumnPair, public constraint: GraphCellConstraint) {}
-}
