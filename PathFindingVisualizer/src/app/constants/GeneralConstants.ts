bad0aa16cb88c5e96259a0ad1f724d18f3dc9e26
Philipp Butz <philipp.butz92@gmail.com>
2021-01-01 18:16:33 +0100

    fix layout

--- PathFindingVisualizer/src/app/constants/GeneralConstants.ts
+++ PathFindingVisualizer/src/app/constants/GeneralConstants.ts
@@ -1,9 +1,9 @@
 export const MOUSE_DRAG_WALL_TIMEOUT_MS = 250;
 export const INITIAL_ALGORITHM_SPEED = 100;
 
-export const INITIAL_NUMBER_OF_HEX_PER_ROW = 20;
+export const INITIAL_NUMBER_OF_HEX_PER_ROW = 30;
 export const HEXAGON_SIZE_PX = 12;
-export const CANVAS_SIZE_PX = 450;
+export const CANVAS_SIZE_PX = 650;
 
 export const NOTIFICATION_SETTINGS = {
   positionClass: 'toast-bottom-right',
