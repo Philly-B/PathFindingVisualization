e669045ab628f7f39ee6dfe22d3c90f8b0ff2f93
Philipp Butz <philipp.butz92@gmail.com>
2021-01-07 11:32:57 +0100

    disable grid size if another button already enabled

--- PathFindingVisualizer/src/app/modules/path-finder/presentation/graph-grid-size/graph-grid-size.component.ts
+++ PathFindingVisualizer/src/app/modules/path-finder/presentation/graph-grid-size/graph-grid-size.component.ts
@@ -2,30 +2,32 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { distinct, switchMap, take } from 'rxjs/operators';
+import { GraphControlMode, GraphControlSettings } from 'src/app/model/GraphControlSettings';
 import { AppState } from 'src/app/store/app.reducer';
 import { reloadGraphState, setGridSize } from 'src/app/store/graph-store/graph.actions';
-import { selectGridSize } from 'src/app/store/graph-store/graph.selectors';
+import { selectGraphControlSettings, selectGridSize } from 'src/app/store/graph-store/graph.selectors';
 
 @Component({
   selector: 'app-graph-grid-size',
   templateUrl: './graph-grid-size.component.html',
-  styleUrls: ['./graph-grid-size.component.scss']
+  styleUrls: ['./graph-grid-size.component.scss'],
 })
 export class GraphGridSizeComponent implements OnInit, OnDestroy {
-
   initialGridSize: number;
   min = 5;
   max = 40;
   stepSize = 5;
 
-  private subscription: Subscription;
+  disabled = false;
+
+  private subscriptions = new Subscription();
 
   constructor(private store: Store<AppState>) {
     this.initialGridSize = -1;
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscriptions.unsubscribe();
   }
 
   ngOnInit(): void {
@@ -34,12 +36,19 @@ export class GraphGridSizeComponent implements OnInit, OnDestroy {
       .pipe(take(1))
       .subscribe((gridSize) => (this.initialGridSize = gridSize));
 
-    this.subscription = this.store
-      .select(reloadGraphState)
-      .pipe(
-        switchMap(() => this.store.select(selectGridSize)),
-        distinct())
-      .subscribe((gridSize) => (this.initialGridSize = gridSize));
+    this.subscriptions.add(
+      this.store
+        .select(reloadGraphState)
+        .pipe(
+          switchMap(() => this.store.select(selectGridSize)),
+          distinct()
+        )
+        .subscribe((gridSize) => (this.initialGridSize = gridSize))
+    );
+
+    this.subscriptions.add(
+      this.store.select(selectGraphControlSettings).subscribe(this.setDisabledBasedOnGraphControls)
+    );
   }
 
   valueChanged = (event) => {
@@ -47,4 +56,11 @@ export class GraphGridSizeComponent implements OnInit, OnDestroy {
     this.store.dispatch(setGridSize({ gridSize }));
   };
 
+  private setDisabledBasedOnGraphControls = (graphControlSettings: GraphControlSettings): void => {
+    this.disabled =
+      graphControlSettings.setStart === GraphControlMode.ENABLED ||
+      graphControlSettings.setEnd === GraphControlMode.ENABLED ||
+      graphControlSettings.modifyWalls === GraphControlMode.ENABLED ||
+      graphControlSettings.removeAllWalls === GraphControlMode.ENABLED;
+  };
 }
